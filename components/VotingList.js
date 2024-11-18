c2c1f5de85fbd761be63c702ec5f1fdb71f0a5fd
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-11-18 16:49:41 +0200

    rudementary listing

--- components/VotingList.js
+++ components/VotingList.js
@@ -1,9 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { app } from './firebaseConfig';
+import { getDatabase, ref, push, onValue } from 'firebase/database';
+
+const database = getDatabase(app);
 
 export default function VotingList() {
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    const itemsRef = ref(database, 'listings/');
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        setListings(Object.values(data));
+      } else {
+        setListings([]); // No items
+      }
+    })
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>This is the list of added series and movies where you can vote them for watching.</Text>
+
+      <FlatList
+        data={listings}
+        renderItem={({ item }) =>
+          <View style={{ flexDirection: "row" }}>
+            <Text>{item.Title}</Text>
+          </View>
+        }
+      />
     </View>
   );
 }
