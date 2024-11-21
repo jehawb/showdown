48026db8d54c34d08a9740fdd5e4fe1962faa73a
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-11-21 16:35:59 +0200

    firebase key finally dug out and combined with the listings

--- components/VotingList.js
+++ components/VotingList.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View, FlatList, Image } from 'react-native';
 import { Button, Card, Text, IconButton, Snackbar, Icon } from "react-native-paper";
 import { useState, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
 import { app } from './firebaseConfig';
 import { getDatabase, ref, push, onValue } from 'firebase/database';
 
@@ -10,12 +11,29 @@ export default function VotingList() {
 
   const [listings, setListings] = useState([]);
 
+  // useEffect(() => {
+  //   const itemsRef = ref(database, 'listings/');
+  //   onValue(itemsRef, (snapshot) => {
+  //     const data = snapshot.val();
+  //     if (data) {
+  //       setListings(Object.values(data));
+  //     } else {
+  //       setListings([]); // No items
+  //     }
+  //   })
+  // }, []);
+
   useEffect(() => {
-    const itemsRef = ref(database, 'listings/');
-    onValue(itemsRef, (snapshot) => {
+    onValue(ref(database, 'listings/'), (snapshot) => {
       const data = snapshot.val();
+
       if (data) {
-        setListings(Object.values(data));
+        const keys = Object.keys(data);
+        // Combine keys with data 
+        const dataWithKeys = Object.values(data).map((obj, index) => {
+          return { ...obj, key: keys[index] }
+        });
+        setListings(dataWithKeys);
       } else {
         setListings([]); // No items
       }
@@ -30,46 +48,42 @@ export default function VotingList() {
     }
   }
 
+  const handleLike = async (item) => {
+    console.log(item.key);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is the list of added series and movies where you can vote them for watching.</Text>
-
       <FlatList
         data={listings}
         renderItem={({ item }) =>
           <Card style={{ marginTop: 15, width: '95%' }}>
-          <Card.Title
-            title={item.Title}
-            subtitle={item.Year}
-            titleVariant="titleLarge"
-          />
-          <Card.Content style={{ flexDirection: 'column', alignItems: "flex-start" }}>
-            <View style={{ flexDirection: 'row', alignItems: "flex-start" }}>
-              <Image
-                source={{ uri: item.Poster }}
-                style={{
-                  width: 150,
-                  height: 250,
-                  resizeMode: 'contain',
-                  marginRight: 15
-                }}
-              />
-              <Text variant="bodyMedium"
-                numberOfLines={15}
-                ellipsizeMode="tail"
-                style={{ flex: 1 }}>{item.Plot}
-              </Text>
-            </View>
-            <Text>Runtime: {item.Runtime}</Text>
-            {item.totalSeasons ? <Text>Seasons: {item.totalSeasons}</Text>: null}
-            <Text>IMDB rating: {item.imdbRating}</Text>
-            <Text>Metacritic score: {item.Metascore}</Text>
-          </Card.Content>
-          <Card.Actions>
-            <IconButton icon="web" onPress={() => handleBrowse(item.imdbID)} />
-            {/* <IconButton icon="plus" onPress={() => handleToAddList()} /> */}
-          </Card.Actions>
-        </Card>
+            <Card.Title
+              title={item.Title}
+              titleVariant="titleLarge"
+            />
+            <Card.Content style={{ flexDirection: 'column', alignItems: "flex-start" }}>
+              <View style={{ flexDirection: 'row', alignItems: "flex-start" }}>
+                <Image
+                  source={{ uri: item.Poster }}
+                  style={{
+                    width: 150,
+                    height: 250,
+                    resizeMode: 'contain',
+                    marginRight: 15
+                  }}
+                />
+              </View>
+            </Card.Content>
+            <Card.Actions>
+              <IconButton icon="web" onPress={() => handleBrowse(item.imdbID)} />
+              {/* If likes array even exists is checked first */}
+              {item.likes && item.likes.includes("USERNAME") ?
+                <IconButton icon="thumb-up" iconColor='orange' onPress={() => handleLike(item)} />
+                :
+                <IconButton icon="thumb-up-outline" onPress={() => handleLike(item)} />}
+            </Card.Actions>
+          </Card>
         }
       />
     </View>
